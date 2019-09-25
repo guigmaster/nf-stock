fe4e882e11e33ad88f784a3235f58f14d118a35b
Guilherme Silva <guisilva.master@gmail.com>
2019-09-25 18:14:19 -0300

    Add react-router-dom to make navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import Routes from './routes';
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
+    <>
+      <Routes />
       <GlobalStyles />
-    </div>
+    </>
   );
 }
 
